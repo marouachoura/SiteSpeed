1b5e7d9d1a1531a44c28f16b6940f7e7bc4e467a
maroua choura <62516341+marouachoura@users.noreply.github.com>
2024-03-01 16:11:57 +0100

    Update ybenali.js

--- ybenali.js
+++ ybenali.js
@@ -8,8 +8,8 @@
 
 export default async function (context, commands) {
 
-  context.log.info('Start to measure my first URL');
+  context.log.info('Start to measure my first URL')
 
-  return commands.measure.start('https://www.sitespeed.io');
+  return commands.measure.start('https://www.sitespeed.io')
 
 }
