d571b489f3d0d551e96f71c00da67f05124f6903
maroua choura <62516341+marouachoura@users.noreply.github.com>
2024-03-21 09:12:51 +0100

    Update script.mjs

--- script.mjs
+++ script.mjs
@@ -16,78 +16,60 @@ export default async function (context, commands) {
     // 1 | open | / |
     const loginUrl = 'http://front-end-faro-smetest.apps.aps.lab/'; 
     // await commands.measure.start('Login Page', { alias: loginUrl });
-  await commands.navigate('http://front-end-faro-smetest.apps.aps.lab/');
+  await commands.navigate('http://front-end-autoinstrument-smetest.apps.aps.lab/');
   await commands.wait.byPageToComplete();
   const seleniumElement = await seleniumDriver.findElement(By.linkText('Login'));
   // 2 | setWindowSize | 1552x840 |
   // await seleniumElement.manage().window().setRect({ width: 1552, height: 840 })
-  await commands.measure.start('Click Login');
 
+  await commands.measure.start('Click Login');
   await seleniumElement.click()
   await commands.wait.byPageToComplete();
-  
-    // 6 | click | id=username-modal |
-  await seleniumElement.findElement(By.xpath("//input[@id='username-modal']")).click()
- 
-  await seleniumElement.findElement(By.xpath("//input[@id='username-modal']")).sendKeys("user1")
-
-  await seleniumElement.findElement(By.xpath("//input[@id='password-modal']")).click()
-  await seleniumElement.findElement(By.xpath("//input[@id='password-modal']")).sendKeys('password')
-
-  await seleniumElement.findElement(By.xpath("//div[@id='login-modal']/div/div/div[2]/form/p/button")).click()
+  await seleniumDriver.findElement(By.xpath("//input[@id='username-modal']")).click()
+  await seleniumDriver.findElement(By.xpath("//input[@id='username-modal']")).sendKeys("user1")
+  await seleniumDriver.findElement(By.xpath("//input[@id='password-modal']")).click()
+  await seleniumDriver.findElement(By.xpath("//input[@id='password-modal']")).sendKeys('password')
+  await seleniumDriver.findElement(By.xpath("//div[@id='login-modal']/div/div/div[2]/form/p/button")).click()
   await commands.wait.byPageToComplete();
   await commands.measure.stop();  
   
+ 
   await commands.measure.start('Open catalogue');
-  await commands.wait.byPageToComplete();
-  // await new Promise(resolve => setTimeout(resolve, 6000));
-  // const element = await seleniumElement.findElement(By.xpath("//*[@id='tabCatalogue']"));
-  // await commands.wait.until(ExpectedConditions.visibilityOf(element));
-  // await element.click();
-  // await seleniumElement.findElement(By.xpath("//*[@id='tabCatalogue']")).click()
-  await commands.navigate('http://front-end-autoinstrument-smetest.apps.aps.lab/category.html')
+  await seleniumDriver.findElement(By.css("#tabCatalogue > a")).click()
   await commands.wait.byPageToComplete();
   await commands.measure.stop();
  
   await commands.measure.start('click on product');
-  // await seleniumElement.findElement(By.xpath("(//a[contains(text(),'View detail')])[2]")).click()
-  await commands.navigate('http://front-end-autoinstrument-smetest.apps.aps.lab/detail.html?id=3395a43e-2d88-40de-b95f-e00e1502085b')
-  
+  await seleniumDriver.findElement(By.xpath("(//a[contains(text(),'View detail')])[2]")).click()  
   await commands.wait.byPageToComplete();
   await commands.measure.stop();
  
-  // await commands.measure.start('Add to cart');
-  // await seleniumElement.findElement(By.id("buttonCart")).click()
-  // const addToCartButton = await seleniumDriver.wait(until.elementToBeClickable(By.id('buttonCart')));
-  //   // Click the "Add to cart" button
-  // await addToCartButton.click();
-  // const ele=driver.findelement(By.xpath("//div[@id='productMain']/div[2]/div/p[3]/a"));
-  // wait.until(ExpectedCondition.Invisiblityofelemntlocated(ele));
-  // await ele.click()
-  // await commands.measure.stop();
- 
-  // await commands.measure.start('Open Cart');
-  // await seleniumElement.findElement(By.xpath("//span[@id='numItemsInCart']")).click()
-  // await commands.wait.byPageToComplete();
-  // await commands.measure.stop();
+  await commands.measure.start('Add to cart');
+  await seleniumDriver.findElement(By.xpath("//div[@id='productMain']/div[2]/div/p[3]/a")).click()
+  await commands.wait.byPageToComplete();
+  await seleniumDriver.findElement(By.xpath("//span[@id='numItemsInCart']")).click()
+  await commands.wait.byPageToComplete();
+  await commands.measure.stop();
  
  
-  // await commands.measure.start('Basket overvoew');
-  // await seleniumElement.findElement(By.xpath('//div[@id=\'basket-overview\']/a')).click()
-  // await commands.wait.byPageToComplete();
-  // await commands.measure.stop();
+  await commands.measure.start('Basket overview');
+  await seleniumDriver.findElement(By.xpath('//div[@id=\'basket-overview\']/a')).click()
+  await commands.wait.byPageToComplete();
+  await commands.measure.stop();
  
   // await commands.measure.start('Update Cart');
-  // await seleniumElement.findElement(By.xpath("//a[@onclick='updateCart()']")).click()
+  // await seleniumDriver.findElement(By.xpath("//a[@onclick='updateCart()']")).click()
   // await commands.measure.stop();
  
-  // await commands.measure.start('Pass order button');
-  // await seleniumElement.findElement(By.xpath("//button[@id='orderButton']")).click()
-  // await commands.wait.byPageToComplete();
-  // await commands.measure.stop();
+  await commands.measure.start('Pass order button');
+  await seleniumDriver.findElement(By.id("orderButton")).click()
+  await seleniumDriver.findElement(By.id("orderButton")).click()
+  await commands.wait.byPageToComplete();
+  await commands.measure.stop();
  
-  // await commands.measure.start('Logout');
-  // await seleniumElement.findElement(By.linkText('Logout')).click()
-  // await commands.measure.stop();
+  await commands.measure.start('Logout');
+  await seleniumDriver.findElement(By.linkText('Logout')).click()
+  await commands.measure.stop();
+
 
 }
